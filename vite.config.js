5746a463aed121fcf8d997e76adb5217b92c33f9
PomMort <hiuhaha789@gmail.com>
2025-04-10 00:52:12 +0700

    fix Ui

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,22 @@ export default defineConfig({
   server: {
     proxy: {
       '/chatHub': {
-        target: 'https://hungnv.iselab.cloud:7221',
-        // target: 'https://localhost:7221',
+        // target: 'https://hungnv.iselab.cloud:7221',
+        target: 'https://localhost:7221',
         changeOrigin: true,
         secure: false,
         ws: true,
         rewriteWsOrigin: true,
       },
       '/chat': {
-        target: 'https://hungnv.iselab.cloud:7221',
+        // target: 'https://hungnv.iselab.cloud:7221',
+        target: 'https://localhost:7221',
         changeOrigin: true,
         secure: false,
       },
       '/api': {
-        target: 'https://hungnv.iselab.cloud:7221',
+        // target: 'https://hungnv.iselab.cloud:7221',
+        target: 'https://localhost:7221',
         changeOrigin: true,
         secure: false
       }
