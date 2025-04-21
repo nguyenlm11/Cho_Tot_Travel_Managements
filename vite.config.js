4a1d119a203b9f6e3bdaee27097ceea6c6c1c722
PomMort <hiuhaha789@gmail.com>
2025-04-21 22:20:00 +0700

    hot fix

--- vite.config.js
+++ vite.config.js
@@ -7,25 +7,25 @@ export default defineConfig({
     proxy: {
       '/chatHub': {
         //Chạy sever deploy link này
-        // target: 'https://hungnv.iselab.cloud:7221/api',
+        target: 'https://hungnv.iselab.cloud:7221/api',
 
-        target: 'http://localhost:7221/api',
+        // target: 'http://localhost:7221/api',
         changeOrigin: true,
         ws: true,
         rewriteWsOrigin: true,
       },
       '/chat': {
         //Chạy sever deploy link này
-        // target: 'https://hungnv.iselab.cloud:7221/api',
+        target: 'https://hungnv.iselab.cloud:7221/api',
 
-        target: 'http://localhost:7221/api',
+        // target: 'http://localhost:7221/api',
         changeOrigin: true,
       },
       '/api': {
         //Chạy sever deploy link này
-        // target: 'https://hungnv.iselab.cloud:7221/api',
+        target: 'https://hungnv.iselab.cloud:7221/api',
         
-        target: 'http://localhost:7221/api',
+        // target: 'http://localhost:7221/api',
         changeOrigin: true,
       }
     }
